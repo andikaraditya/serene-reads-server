fe53818b69ab93c0635560f52f04617f5139f7db
Andika <andikaraditya1@gmail.com>
2023-10-21 13:12:54 +0700

    feat: add get news feature
    - add new endpoint to get news from news api
    - news retrieved will have topic related to books and literature

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,13 @@ const express = require("express")
 const router = express.Router()
 const books = require('./books')
 const Auth = require('../controllers/auth');
+const News = require('../controllers/news');
 
 router.post("/login", Auth.login)
 
 router.post("/register", Auth.register)
+
+router.get("/news", News.getNews)
 
 router.use("/books", books)
 
