aa565e4e09928dec3d16c3e9a5d612b0315e83e0
Andika <andikaraditya1@gmail.com>
2023-10-25 19:14:30 +0700

    deployment setup

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const router = require("./routes")
 const errorhandler = require("./middlewares/errorhandler")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.urlencoded())
